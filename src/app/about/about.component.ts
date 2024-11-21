0b18d65fbbb11240a3be4446d43b07abbe8a4168
Aline <alinebarantes@gmail.com>
2024-11-21 03:17:35 -0300

    WIP added movie header, video trailer, and more info on the description

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieService } from '../services/movieapi.service';
 import { ActivatedRoute } from '@angular/router';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
   movieDetails: any;
+  safeVideoUrl: SafeResourceUrl | null = null;
+  director: string = '';
+  writers: string[] = [];
 
-  constructor(private route: ActivatedRoute, private movieApi: MovieService) {
-
-  }
+  constructor(
+    private route: ActivatedRoute,
+    private movieApi: MovieService,
+    private sanitizer: DomSanitizer
+  ) {}
 
   ngOnInit(): void {
     const movieId = this.route.snapshot.paramMap.get('id');
@@ -20,15 +26,27 @@ export class AboutComponent implements OnInit {
       this.movieApi.getMovieDetails(+movieId).subscribe(
         (data) => {
           this.movieDetails = data;
+          if (this.movieDetails.videos && this.movieDetails.videos.results.length > 0) {
+            const videoKey = this.movieDetails.videos.results[0].key;
+            this.safeVideoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${videoKey}`);
+          }
+          this.extractCrewInfo();
         },
         (error) => {
           console.error('Error fetching movie details:', error);
-          // Handle error (e.g., show error message to user)
         }
       );
     } else {
       console.error('No movie ID provided');
-      // Handle error (e.g., redirect to home page)
+    }
+  }
+
+  extractCrewInfo(): void {
+    if (this.movieDetails && this.movieDetails.credits && this.movieDetails.credits.crew) {
+      this.director = this.movieDetails.credits.crew.find((member: any) => member.job === 'Director')?.name || 'Unknown';
+      this.writers = this.movieDetails.credits.crew
+        .filter((member: any) => member.department === 'Writing')
+        .map((writer: any) => writer.name);
     }
   }
 }
